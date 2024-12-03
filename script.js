7f3d105ec2b74006a05bf23064ff8fcc73de8b92
Renan <renanribeiro.mcsd@gmail.com>
2024-12-03 18:20:25 -0300

    terceiro-deploy

--- script.js
+++ script.js
@@ -51,3 +51,22 @@ function pad(n) {
     return (n < 10 ? '0' : '') + n;
 }
 // Relogio
+
+
+const script_do_google = 'https://script.google.com/macros/s/AKfycbyjTu4swo2FHK6ebuZvTcZIglrzD1h9ppm9mha3R0n8zEBNY7L0AgWb5piO9maDlWAT/exec';
+const dados_do_formulario = document.forms['formulario-participantes'];
+
+dados_do_formulario.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    fetch(script_do_google, { method: 'POST', body: new FormData(dados_do_formulario) })
+        .then(response => {
+            // Se os dados forem gravados corretamente, será enviada uma mensagem de sucesso
+            alert('Dados enviados com sucesso!', response);
+            dados_do_formulario.reset(); 
+        })
+        .catch(error => {
+            // Se houver erro no envio, a menssagem abaixo será exibida
+            console.error('Erro no envio dos dados!', error);
+        });
+});
